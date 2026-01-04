34a764a0bde6d200f7083d92cced64f12e1d863c
benfordi25 <ben_ford123@hotmail.co.uk>
2026-01-04 14:29:42 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,14 @@
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open("fps-report-v1").then((cache) =>
+    caches.open("fps-report-v2").then((cache) =>
       cache.addAll(["./", "./index.html", "./app.js", "./manifest.webmanifest"])
     )
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(self.clients.claim());
-});
+self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
 });
