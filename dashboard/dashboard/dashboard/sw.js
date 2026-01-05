bc7df8ba3e472410aa5c78c5f971574540766f3a
benfordi25 <ben_ford123@hotmail.co.uk>
2026-01-05 13:51:10 +0000

    Update sw.js

--- dashboard/dashboard/dashboard/sw.js
+++ dashboard/dashboard/dashboard/sw.js
@@ -1,1 +1,31 @@
+const CACHE = "fps-dashboard-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js"
+];
 
+// Install
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).catch(() => caches.match("./")))
+  );
+});
